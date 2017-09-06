7ddad68fba96c29c096c275dc253ebf9a697f739
liu520yan <1027552153@qq.com>
2017-09-06 19:49:28 +0800

    设计模式

---
+++ design/src/main/java/com/liuyan/study/design/context/Context.java
@@ -0,0 +1,18 @@
+package com.liuyan.study.design.context;
+
+
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+/**
+ * Created by liuyan on 2017/9/5.
+ */
+public class Context {
+    public static void main(String[] args) {
+//        Writer writer = new Writer();
+//        writer.setName("Writer's name");
+//        StandardEvaluationContext modifierContext = new StandardEvaluationContext(subscriberContext);
+//        modifierContext.setVariable("name", "Overriden writer's name");
+//        parser.parseExpression("name = #name").getValue(modifierContext);
+//        System.out.println("writer's name is : " + writer.getName());
+    }
+}
---
+++ design/src/main/java/com/liuyan/study/design/context/Writer.java
@@ -0,0 +1,12 @@
+package com.liuyan.study.design.context;
+
+import lombok.Data;
+
+/**
+ * Created by liuyan 2017/9/5.
+ */
+@Data
+public class Writer {
+    String name;
+
+}
