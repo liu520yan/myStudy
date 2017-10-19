31570804d170f9483095b64397801a0b956fd367
liuyan <yan.liu@@timanetworks.com>
2017-10-19 17:00:24 +0800

    add algorithm module 最大公约数 算法

---
+++ algorithm/src/main/java/com/liuyan/study/GreatestCommonDivisor.java
@@ -0,0 +1,31 @@
+package com.liuyan.study;
+
+/**
+ * Created by liuyan on 2017/10/19.
+ */
+public class GreatestCommonDivisor {
+
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(12345, 1));
+    }
+
+    /**
+     * 获取最大公约数
+     * <p>
+     * 传入 p q 两个费 负整数。
+     * q 等于0 时 ，最大公约数为p
+     * p对q取余得到c
+     * q和c的最大公约数为p和q 的最大公约数
+     *
+     * @param p
+     * @param q
+     * @return
+     */
+    public static int getGreatestCommonDivisor(int p, int q) {
+        if (q == 0) {
+            return p;
+        }
+        int c = p % q;
+        return getGreatestCommonDivisor(q, c);
+    }
+}
