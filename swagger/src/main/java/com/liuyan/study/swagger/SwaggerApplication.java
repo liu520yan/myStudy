f964915668f832b4ec2507cc4e68fc23bf386d98
liuyan <yan.liu@@timanetworks.com>
2017-12-07 15:41:17 +0800

    swagger； swagger 生成pdf； spring boot 热部署

---
+++ swagger/src/main/java/com/liuyan/study/swagger/SwaggerApplication.java
@@ -0,0 +1,14 @@
+package com.liuyan.study.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liuyan on 2017/12/7.
+ */
+@SpringBootApplication
+public class SwaggerApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SwaggerApplication.class, args);
+    }
+}
