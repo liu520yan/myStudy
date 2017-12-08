1fae7ce3fc7cecc6a6a2c35c217f40a04294eb3c
liuyan <yan.liu@@timanetworks.com>
2017-12-08 15:57:18 +0800

    swagger 生成静态文档

--- swagger/src/main/java/com/liuyan/study/swagger/controller/ComputeController.java
+++ swagger/src/main/java/com/liuyan/study/swagger/controller/ComputeController.java
@@ -1,8 +1,6 @@
 package com.liuyan.study.swagger.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,31 +9,43 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by liuyan on 2017/12/7.
  */
-@Api(value = "计算服务",description="简单的计算服务，提供加减乘除运算API")
+@Api(value = "计算服务", description = "简单的计算服务，提供加减乘除运算API")
 @RestController
 @RequestMapping("/compute")
 public class ComputeController {
-    @ApiOperation("加法运算")
+    @ApiOperation(value = "加法运算")
     @PostMapping("/add")
-    public Double add(@RequestParam Double a, @RequestParam Double b) {
+//    @ApiImplicitParams({@ApiImplicitParam(name = "a", value = "加数", required = true, dataType = "Double", paramType = "query"),
+//            @ApiImplicitParam(name = "b", value = "加数", required = true, dataType = "Double", paramType = "query")})
+    public Double add(@RequestParam @ApiParam(name = "a", value = "加数", required = true) Double a
+            , @ApiParam(name = "b", value = "加数", required = true) @RequestParam Double b) {
         return a + b;
     }
 
-    @ApiOperation("减法运算")
+    @ApiOperation(value = "减法运算")
     @PostMapping("/sub")
-    public Double sub(@RequestParam Double a,  @RequestParam Double b) {
+//    @ApiImplicitParams({@ApiImplicitParam(name = "a", value = "被减数", required = true, dataType = "Double", paramType = "query"),
+//            @ApiImplicitParam(name = "b", value = "减数", required = true, dataType = "Double", paramType = "query")})
+    public Double sub(@RequestParam @ApiParam(name = "a", value = "被减数", required = true) Double a,
+                      @ApiParam(name = "b", value = "减数", required = true) @RequestParam Double b) {
         return a - b;
     }
 
-    @ApiOperation("乘法运算")
+    @ApiOperation(value = "乘法运算")
     @PostMapping("/mul")
-    public Double mul(@RequestParam Double a,  @RequestParam Double b) {
+//    @ApiImplicitParams({@ApiImplicitParam(name = "a", value = "被乘数", required = true, dataType = "Double", paramType = "query"),
+//            @ApiImplicitParam(name = "b", value = "乘数", required = true, dataType = "Double", paramType = "query")})
+    public Double mul(@RequestParam @ApiParam(name = "a", value = "被乘数", required = true) Double a,
+                      @ApiParam(name = "b", value = "乘数", required = true) @RequestParam Double b) {
         return a * b;
     }
 
-    @ApiOperation("除法运算")
+    @ApiOperation(value = "除法运算")
     @PostMapping("/div")
-    public Double div(@ApiParam("被除数")@RequestParam Double a, @ApiParam("除数")@RequestParam   Double b) {
+//    @ApiImplicitParams({@ApiImplicitParam(name = "a", value = "被除数", required = true, dataType = "Double", paramType = "query"),
+//            @ApiImplicitParam(name = "b", value = "除数", required = true, dataType = "Double", paramType = "query")})
+    public Double div(@ApiParam(name = "a", value = "被除数", required = true) @RequestParam Double a
+            , @ApiParam(name = "b", value = "除数", required = true) @RequestParam Double b) {
         return a / b;
-        }
-        }
+    }
+}
