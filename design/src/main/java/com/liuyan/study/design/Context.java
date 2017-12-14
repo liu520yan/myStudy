d22978d7105f7e146345cbad7354d0b401a7b2eb
liuyan <yan.liu@@timanetworks.com>
2017-12-14 18:29:07 +0800

    设计模式

--- design/src/main/java/com/liuyan/study/design/Context.java
+++
@@ -1,9 +0,0 @@
-package com.liuyan.study.design;
-
-/**
- * 解释设计模式
- * Created by liuyan on 2017/9/5.
- */
-public class Context {
-
-}
---
+++ design/src/main/java/com/liuyan/study/design/decorator/Decorator.java
@@ -0,0 +1,8 @@
+package com.liuyan.study.design.decorator;
+
+/**
+ * Created by liuyan on 2017/12/14.
+ */
+public class Decorator extends Component {
+
+}
